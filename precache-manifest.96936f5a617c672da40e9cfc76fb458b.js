972efc6d867834179a9620b2c7b53538faad844f
Hamza Sayyed <hamzawp404@gmail.com>
2022-10-11 01:07:34 +0530

    Updates

---
+++ precache-manifest.96936f5a617c672da40e9cfc76fb458b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d8bf5e29ed15d3f42b8",
+    "url": "/Hamzawp/Tasktracker/static/css/main.370b11a6.chunk.css"
+  },
+  {
+    "revision": "1d8bf5e29ed15d3f42b8",
+    "url": "/Hamzawp/Tasktracker/static/js/main.1d8bf5e2.chunk.js"
+  },
+  {
+    "revision": "a6b0c181771552b73dbd",
+    "url": "/Hamzawp/Tasktracker/static/js/1.a6b0c181.chunk.js"
+  },
+  {
+    "revision": "e182dbb5096e6ba3faf4",
+    "url": "/Hamzawp/Tasktracker/static/js/2.e182dbb5.chunk.js"
+  },
+  {
+    "revision": "a355eb5fbccefe256e77",
+    "url": "/Hamzawp/Tasktracker/static/js/runtime~main.a355eb5f.js"
+  },
+  {
+    "revision": "d5b5cac988641e1d9b346cfbb99e0a35",
+    "url": "/Hamzawp/Tasktracker/static/media/paul-volkmer-qVotvbsuM_c-unsplash.d5b5cac9.jpg"
+  },
+  {
+    "revision": "2bb6df0d5e7b214c5a8270f007baf019",
+    "url": "/Hamzawp/Tasktracker/index.html"
+  }
+];
