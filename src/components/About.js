a4e6d43390f188cf602968143869317c223dccd7
Hamza Sayyed <hamzawp404@gmail.com>
2022-10-11 00:55:36 +0530

    Added folder

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      <h4>Version 1.0.2</h4>
+      <h5>Practised by Hamza Sayyed</h5>
+      <Link to='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export default About
